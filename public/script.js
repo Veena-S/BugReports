15285b2f4c9a11aa6676dffb1eb5a87d15ca77c4
Veena-S <veenasivan@gmail.com>
2020-12-22 21:37:18 +0800

    Modified to display the newly created bug in the page, after hiding the form

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
+// To hold the form element for creating new bug
+// This variable is made global, to support toggling
+let divFormElement = null;
+// To hold the list of newly created bugs.
+let divNewBugsList = null;
+
 /**
  * Function to create a new "div" element and returns the same.
  * It also appends the created div element to the document body.
  */
-const createAndAppendDivElement = () => {
+const createAndAppendDivElement = (data = '') => {
   const divElement = document.createElement('div');
+  divElement.innerHTML = data;
   document.body.appendChild(divElement);
   return divElement;
 };
@@ -45,6 +52,45 @@ const createSubmitButton = (txtOnButton, clickHandlerFn) => {
 };
 
 /**
+ * Function to hide/show the form element to create new bug
+ * @param {String} displayStyle - The style display property to be set on the element
+ */
+const clearAndToggleNewBugForm = (displayStyle) => {
+  document.getElementById('problem-desc').value = '';
+  document.getElementById('error-text').value = '';
+  document.getElementById('git-commit').value = '';
+  divFormElement.style.display = displayStyle;
+};
+
+const displayBugData = (bugData) => {
+  if (divNewBugsList === null)
+  {
+    divNewBugsList = createAndAppendDivElement();
+  }
+  /*
+  console.log(`new bug id: ${response.data.id}`);
+      console.log(`new bug problem: ${response.data.problem}`);
+      console.log(`new bug error text: ${response.data.errorText}`);
+      console.log(`new bug commit: ${response.data.commit}`);
+  */
+  const pIDEl = document.createElement('p');
+  pIDEl.innerHTML = `Id: ${bugData.id}`;
+  divNewBugsList.appendChild(pIDEl);
+
+  const pProblemEl = document.createElement('p');
+  pProblemEl.innerHTML = `Problem: ${bugData.problem}`;
+  divNewBugsList.appendChild(pProblemEl);
+
+  const pErrorEl = document.createElement('p');
+  pErrorEl.innerHTML = `Id: ${bugData.errorText}`;
+  divNewBugsList.appendChild(pErrorEl);
+
+  const pCommitEl = document.createElement('p');
+  pCommitEl.innerHTML = `Id: ${bugData.commit}`;
+  divNewBugsList.appendChild(pCommitEl);
+};
+
+/**
  * Function that post the info on new bug to the server using Axios
  */
 const postNewBug = () => {
@@ -59,17 +105,20 @@ const postNewBug = () => {
   // Make the POST request to create the Bug
   axios.post('/createBug', data)
     .then((response) => {
-      console.log(`Response: ${response.data}`);
       console.log(`new bug id: ${response.data.id}`);
       console.log(`new bug problem: ${response.data.problem}`);
       console.log(`new bug error text: ${response.data.errorText}`);
       console.log(`new bug commit: ${response.data.commit}`);
-      console.log(response.status);
-      console.log(response.statusText);
-      console.log(response.headers);
-      console.log(response.config);
+
+      // console.log(`Response: ${response.data}`);
+      // console.log(response.status);
+      // console.log(response.statusText);
+      // console.log(response.headers);
+      // console.log(response.config);
 
       // After getting the response, hide the form elements and show the list of bugs
+      clearAndToggleNewBugForm('none');
+      displayBugData(response.data);
     })
     .catch((error) => {
       console.log(error);
@@ -80,7 +129,14 @@ const postNewBug = () => {
  * Function to create all the elements to submit a new bug
  */
 const createNewBugForm = () => {
-  const divFormElement = createAndAppendDivElement();
+  if (divFormElement === null)
+  {
+    divFormElement = createAndAppendDivElement();
+  }
+  else {
+    clearAndToggleNewBugForm('block');
+    return divFormElement;
+  }
   // Create problem elemet
   const problemEl = createInputTextElement('Problem: ', 'problem-desc');
   divFormElement.appendChild(problemEl);
